3dce4da41b283adcf6246842dfa8825bfe60ffc9
Jlevan25 <91471603+Jlevan25@users.noreply.github.com>
2022-05-25 16:47:46 +0300

    config

--- configs/config.py
+++ configs/config.py
@@ -3,8 +3,11 @@ import time
 
 
 class Config(object):
-    def __init__(self, batch_size, lr, dataset_name, model_name, ROOT_DIR=None, debug=True, show_each=1, device='cpu'):
+    def __init__(self, batch_size, lr, dataset_name, model_name, out_features, ROOT_DIR=None, DATASET_DIR=None,
+                 debug=True,
+                 show_each=1, device='cpu', seed=None, overfit=False):
         self.ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) if ROOT_DIR is None else ROOT_DIR
+        self.DATASET_DIR = DATASET_DIR
         self.debug = debug
         self.batch_size = batch_size
         self.lr = lr
@@ -12,10 +15,13 @@ class Config(object):
         self.dataset_name = dataset_name
         self.model_name = model_name
         self.device = device
+        self.overfit = overfit
+        self.seed = seed
+        self.out_features = out_features
 
         experiment_name = f'model_{self.model_name}_batch_size{self.batch_size}_lr_{self.lr}_{time.time()}'
 
         self.SAVE_PATH = os.path.join(self.ROOT_DIR, 'checkpoints', self.model_name,
-                                      self.dataset_name, str(time.time()))
+                                      self.dataset_name, experiment_name)
 
         self.LOG_PATH = os.path.join(self.ROOT_DIR, 'logs', self.dataset_name, experiment_name)

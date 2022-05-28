623ac5285df35978031d512a56e99d28e21c4e29
Jlevan25 <91471603+Jlevan25@users.noreply.github.com>
2022-05-28 11:44:58 +0300

    config v2.0

--- configs/config.py
+++ configs/config.py
@@ -1,16 +1,18 @@
 import os
-import time
 
 
 class Config(object):
-    def __init__(self, batch_size, lr, dataset_name, model_name, out_features, ROOT_DIR=None, DATASET_DIR=None,
-                 debug=True,
+    def __init__(self, batch_size, lr, dataset_name, model_name, out_features, DATASET_DIR, ROOT_DIR=None,
+                 debug=True, write_by_class_metrics: bool = True, weight_decay=None, momentum=None,
                  show_each=1, device='cpu', seed=None, overfit=False):
         self.ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) if ROOT_DIR is None else ROOT_DIR
         self.DATASET_DIR = DATASET_DIR
         self.debug = debug
+        self.write_by_class_metrics = write_by_class_metrics
         self.batch_size = batch_size
         self.lr = lr
+        self.weight_decay = weight_decay
+        self.momentum = momentum
         self.show_each = show_each
         self.dataset_name = dataset_name
         self.model_name = model_name
